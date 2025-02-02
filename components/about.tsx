626e7668ed34a90586ab78017a91c28bdad061fb
Berke Kanber <berkekanber@gmail.com>
2025-02-02 22:39:44 +0300

    refactor: about page added avatar

--- components/about.tsx
+++ components/about.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import CvButton from "@/components/cv-button";
+import Image from "next/image";
+import Avatar from "../public/chefberke.png";
 
 function About() {
   return (
@@ -8,7 +10,16 @@ function About() {
         <h2 className="text-xl font-medium">About</h2>
       </div>
       <div className="pt-7">
-        <p className="font-light text-gray-200">
+        <div className="flex items-center gap-2">
+          <div>
+            <Image src={Avatar} width={60} height={60} alt="chefberke-avatar" />
+          </div>
+          <div className="flex flex-col">
+            <h2 className="font-medium">Berke Kanber</h2>
+            <p className="font-light text-neutral-300">Software Developer</p>
+          </div>
+        </div>
+        <p className="font-light text-gray-200 pt-7">
           Hi, I&apos;m Berke Kanber, a passionate software developer based in
           İzmir, Turkey. Currently, I&apos;m pursuing my second year in Computer
           Programming at İzmir University of Economics . I love exploring and
